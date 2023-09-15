a9930764b4253b3b5cf5ffae241d0a944131a22d
Joe Masilotti <joe@masilotti.com>
2023-09-15 11:59:07 -0700

    Move ProposalResult to its own file

--- Sources/TurboNavigationDelegate.swift
+++ Sources/TurboNavigationDelegate.swift
@@ -59,15 +59,3 @@ public extension TurboNavigationDelegate {
 
     func sessionDidLoadWebView(_ session: Session) {}
 }
-
-/// Return from `handle(proposal:)` to route a custom controller.
-public enum ProposalResult : Equatable {
-    /// Route a `VisitableViewController`.
-    case accept
-    
-    /// Route a custom `UIViewController` or subclass
-    case acceptCustom(UIViewController)
-
-    /// Do not route. Navigation is not modified.
-    case reject
-}
