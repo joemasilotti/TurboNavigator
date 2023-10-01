47a5415df14871722249e23214407900c0e527c4
Joe Masilotti <joe@masilotti.com>
2023-10-01 06:59:56 -0700

    Clean up demo project (#56)

--- Demo/Demo/AppDelegate.swift
+++ Demo/Demo/AppDelegate.swift
@@ -1,10 +1,4 @@
 import UIKit
 
 @main
-class AppDelegate: UIResponder {}
-
-extension AppDelegate: UIApplicationDelegate {
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-}
+class AppDelegate: UIResponder, UIApplicationDelegate {}
--- Demo/Demo/SceneDelegate.swift
+++ Demo/Demo/SceneDelegate.swift
@@ -1,35 +1,25 @@
 import Turbo
 import TurboNavigator
 import UIKit
-import WebKit
+
+let baseURL = URL(string: "http://localhost:3000")!
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = scene as? UIWindowScene
-        else { fatalError("Expected a UIWindowScene.") }
-
-        createWindow(in: windowScene)
-    }
-
-    // MARK: - Private
-
-    private let baseURL = URL(string: "http://localhost:3000")!
     private lazy var turboNavigator = TurboNavigator(delegate: self, pathConfiguration: pathConfiguration)
     private lazy var pathConfiguration = PathConfiguration(sources: [
         .server(baseURL.appendingPathComponent("/configuration"))
     ])
 
-    private func createWindow(in windowScene: UIWindowScene) {
-        let window = UIWindow(windowScene: windowScene)
-        window.backgroundColor = .white
-        self.window = window
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = scene as? UIWindowScene else { return }
 
-        window.makeKeyAndVisible()
-        window.rootViewController = turboNavigator.rootViewController
+        self.window = UIWindow(windowScene: windowScene)
+        self.window?.makeKeyAndVisible()
 
-        turboNavigator.route(baseURL)
+        self.window?.rootViewController = self.turboNavigator.rootViewController
+        self.turboNavigator.route(baseURL)
     }
 }
 
